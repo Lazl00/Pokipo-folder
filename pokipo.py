e73b8b852e6addcb1b9b2064b6b68b5590e332f7
Laslo COURNOYER <109831203+Lazl00@users.noreply.github.com>
2024-05-04 17:08:54 +0200

    Ajout des dashes "auto" et début de la physique de ralentissemtn directionnel

--- pokipo.py
+++ pokipo.py
@@ -22,7 +22,7 @@ class Joueur:
         self.grounded = True
         self.double_saut_dispo = True
         self.marche = False
-        self.direction=-1 # 0 gauche    1 droite
+        self.direction=-1 # -1 gauche    1 droite
         self.court=False
         self.cd_course=0
         self.court_max=False
@@ -31,49 +31,103 @@ class Joueur:
         self.airdash_dispo=True
         self.jauge_airdash=0
         self.est_en_airdash=False
+        self.auto_airdash=False
+        self.vel_airdash=0
 
         # états du dash
         self.dash_dispo=True
         self.jauge_dash=0
         self.cd_dash=0
+        self.cd_dashmax=0
         self.est_en_dash=False
+        self.auto_dash=False
+        self.auto_dashmax=False
+        self.vel_dash=0
         """"""""""""""""""""""""""""""
         """"""""""""""""""""""""""""""
         """"""""""""""""""""""""""""""
+
+    def move(self):
+        key_dir = 0
+        if pyxel.btn(pyxel.KEY_Q):
+            key_dir = -1
+        elif pyxel.btn(pyxel.KEY_D):
+            key_dir = 1
+
+        if key_dir != 0:
+            if not self.grounded:
+                if self.direction != key_dir:
+                           self.vel_x = key_dir * 0.3
+                else:
+                        self.vel_x = key_dir*self.vitesse
+            else:
+                self.direction = key_dir
+                self.vel_x = self.direction*self.vitesse
+                self.marche = True
+        else:
+                self.vel_x = 0
+                self.marche = False
+
     def update(self):
         self.gravite=0.2
         self.vitesse=1
         self.court=False
         self.court_max=False
         
+        if self.auto_airdash == True:
+            self.gravite = 0
+            self.vel_airdash = self.direction*4-self.vel_x
+            self.jauge_airdash += 1
+            if self.jauge_airdash >= 10:
+                self.auto_airdash = False
+                self.airdash_dispo = False
+        
+        if self.auto_dash == True:
+            self.vel_dash = self.direction*3
+            self.cd_dash+=1
+            if self.cd_dash>=10:
+                 self.dash_dispo=False
+                 self.auto_dash=False
 
-        
+
+             
+
         # Gestion des déplacements horizontaux
-        if pyxel.btn(pyxel.KEY_Q):
-            self.vel_x = -self.vitesse
-            self.marche = True
-            self.direction = -1
-        elif pyxel.btn(pyxel.KEY_D):
-            self.vel_x = self.vitesse
-            self.marche = True
-            self.direction = 1
-        else:
-            self.vel_x = 0
-            self.marche = False
-        
+        self.move()
+
+        if not self.auto_airdash:
+            if self.vel_airdash>=0:
+                self.vel_airdash-=0.2
+                if self.vel_airdash<=0:
+                    self.vel_airdash=0
+            else:
+                self.vel_airdash+=0.2
+                if self.vel_airdash>=0:
+                    self.vel_airdash=0
+
+        if not self.auto_dash:
+            if self.vel_dash>=0:
+                self.vel_dash-=0.2
+                if self.vel_dash<=0:
+                    self.vel_dash=0
+            else:
+                self.vel_dash+=0.2
+                if self.vel_dash>=0:
+                    self.vel_dash=0
+
          # Gestion du sprint
-        if pyxel.btn(pyxel.KEY_CTRL) and pyxel.btn(pyxel.KEY_Q):
-            self.vel_x = 1.5 * self.direction * self.vitesse
-            self.court=True
-            self.cd_course+=1
-        if pyxel.btn(pyxel.KEY_CTRL) and pyxel.btn(pyxel.KEY_D):
-            self.vel_x = 1.2 * self.direction * self.vitesse              #oui je sais, rip. c'est overcompliqué pour rien mais c'était plus simple a coder (modifie stv)
-            self.court=True
-            self.cd_course+=1
+        if pyxel.btn(pyxel.KEY_CTRL):
+            self.court = True
+            self.move()
+            if self.vel_x !=0:
+                self.vel_x*=1.2
+                self.cd_course += 1
         if self.cd_course>=50 and self.court:
-            self.vel_x = 2 * self.direction * self.vitesse
-            self.court_max=True
-        if not self.court:
+            self.move()
+            self.vel_x *= 2
+            if self.vel_x !=0:
+                self.court_max=True
+        elif not self.court:
             self.cd_course=0
             self.court_max=False
 
@@ -89,32 +143,11 @@ class Joueur:
                 
         # Gestion du air dash
         if pyxel.btn( pyxel.MOUSE_BUTTON_RIGHT) and not self.grounded and self.airdash_dispo :
-            self.gravite=0
-            self.vel_x = self.direction*6
-            self.jauge_airdash += 1
-            if self.jauge_airdash>=10:
-                self.airdash_dispo = False
+            self.auto_airdash=True
 
-        
         # Gestion du dash
         if pyxel.btn( pyxel.MOUSE_BUTTON_RIGHT) and self.grounded and self.dash_dispo :
-            if not self.court_max:
-                 self.vel_x = self.direction*4
-            if self.court_max:
-                 self.vel_x = self.direction*5
-            self.jauge_dash += 1
-            if self.jauge_dash >= 10:
-                self.dash_dispo = False
-        
-        if pyxel.btn( pyxel.MOUSE_BUTTON_RIGHT) and self.grounded and self.dash_dispo:
-             self.est_en_dash=True
-        else:
-             self.est_en_dash=False
-
-        if pyxel.btn( pyxel.MOUSE_BUTTON_RIGHT) and not self.grounded and self.airdash_dispo:
-             self.est_en_airdash=True
-        else:
-             self.est_en_airdash=False
+            self.auto_dash=True
 
         # Gestion du fast fall
         if pyxel.btn(pyxel.KEY_S) and not self.grounded:
@@ -124,6 +157,8 @@ class Joueur:
         self.vel_y += self.gravite
 
         # Mise à jour de la position
+        self.x += self.vel_dash
+        self.x += self.vel_airdash
         self.x += self.vel_x
         self.y += self.vel_y
 
@@ -135,6 +170,7 @@ class Joueur:
             self.double_saut_dispo = True
             self.airdash_dispo = True
             self.jauge_airdash=0
+            self.auto_airdash=False
             if not self.dash_dispo:
                 self.jauge_dash=0
                 self.cd_dash += 1
@@ -149,36 +185,41 @@ class Joueur:
 
     def draw(self):
         # Dessiner le joueur
-        if self.court and self.grounded and not self.court_max:                                             #anim début de course (pré-glissade)
+        if self.court and self.grounded and not self.court_max and pyxel.btn( pyxel.KEY_D):#anim début de course (pré-glissade)
+                if pyxel.frame_count % 8 < 4:
+                    pyxel.blt(self.x, self.y, 0, 16, 64, self.direction*self.largeur, self.hauteur, 2)
+                else:
+                    pyxel.blt(self.x, self.y, 0, 24, 64, self.direction*self.largeur, self.hauteur, 2)
+        if self.court and self.grounded and not self.court_max and pyxel.btn( pyxel.KEY_Q):#anim début de course (pré-glissade)
                 if pyxel.frame_count % 8 < 4:
                     pyxel.blt(self.x, self.y, 0, 16, 64, self.direction*self.largeur, self.hauteur, 2)
                 else:
                     pyxel.blt(self.x, self.y, 0, 24, 64, self.direction*self.largeur, self.hauteur, 2)
 
-        if self.court_max and self.grounded:                                                                #anim course max (glissade)
+        if self.court_max and self.grounded:                                                          #anim course max (glissade)
                     pyxel.blt(self.x, self.y, 0, 16, 72, self.direction*self.largeur, self.hauteur, 2)
 
-        if not self.grounded and not self.est_en_airdash:                                                  #anim vol
+        if not self.grounded and not self.auto_airdash:                                               #anim vol
                 if pyxel.frame_count % 8 < 4:
                     pyxel.blt(self.x, self.y, 0, 16, 16, self.direction*self.largeur, self.hauteur, 2)
                 else:
                     pyxel.blt(self.x, self.y, 0, 24, 16, self.direction*self.largeur, self.hauteur, 2)
 
-        if self.marche == False and self.grounded:                                                          #anim statique
+        if self.marche == False and self.grounded:                                                    #anim statique
                 if pyxel.frame_count % 120 < 60:
                     pyxel.blt(self.x, self.y, 0, 0, 72, self.direction*self.largeur, self.hauteur, 2)
                 else:
                     pyxel.blt(self.x, self.y, 0, 8, 72, self.direction*self.largeur, self.hauteur, 2)
 
-        if self.marche == True and self.grounded and not self.court:                                       #anim marche
+        if self.marche == True and self.grounded and not self.court:                         #anim marche
             if pyxel.frame_count % 20 < 10:
                 pyxel.blt(self.x, self.y, 0, 0, 64, self.direction*self.largeur, self.hauteur, 2)
-            elif not self.est_en_dash:
+            elif not self.auto_dash:
                 pyxel.blt(self.x, self.y, 0, 8, 64, self.direction*self.largeur, self.hauteur, 2)
 
-        if self.est_en_dash:               #anim dash
+        if self.auto_dash:                                                               #anim dash
              pyxel.blt(self.x, self.y, 0, 0, 80, self.direction*self.largeur, self.hauteur, 2)
-        if self.est_en_airdash:               #anim airdash
+        if self.auto_airdash:                                                              #anim airdash
              pyxel.blt(self.x, self.y, 0, 0, 80, self.direction*self.largeur, self.hauteur, 2)
         
 
@@ -205,5 +246,5 @@ class App:
         else:
             pyxel.camera(pos_x-80,0)
         pyxel.text(10,10, "Ladies, with gentle hands", 3)
-
+        
 App()
